524b2d19b30c862b592bfd7a3f378be063e5b527
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2023-10-05 18:15:45 -0300

    first commit

---
+++ src/config/constants.js
@@ -0,0 +1,8 @@
+const jsonSecret = process.env.JSON_SCREAT;
+
+const imageMimeType = ["image/png", "image/jpeg", "image/jpg"];
+
+export default {
+  jsonSecret,
+  imageMimeType,
+};
---
+++ src/test/routes/editorasRoutes.test.js
@@ -0,0 +1,86 @@
+import request from "supertest";
+import { test, describe, expect, it, jest } from "@jest/globals";
+import app from "../../app.js";
+
+let server;
+// eslint-disable-next-line no-undef
+beforeEach(() => {
+  const port = 3000;
+  server = app.listen(port);
+});
+
+// eslint-disable-next-line no-undef
+afterEach(() => {
+  server.close();
+});
+
+describe("GET em /editoras", () => {
+  it("Deve retornar uma lista de editoras", async () => {
+    const resposta = await request(app)
+      .get("/editoras")
+      .set("Accept", "application/json")
+      .expect("content-type", /json/)
+      .expect(200);
+
+    expect(resposta.body[0].email).toEqual("e@e.com");
+  });
+});
+
+let idResposta;
+describe("POST em /editoras", () => {
+  it("Deve adicionar uma nova editora", async () => {
+    const resposta = await request(app)
+      .post("/editoras")
+      .send({
+        nome: "CDC",
+        cidade: "São Paulo",
+        email: "s@s.com",
+      })
+      .expect(201);
+
+    idResposta = resposta.body.content.id;
+  });
+  it("Deve não adicionar nada ao passar o body vazio", async () => {
+    await request(app).post("/editoras").send({}).expect(400);
+  });
+});
+
+describe("PUT em /editoras/id", () => {
+  test.each([
+    ["nome", { nome: "ACD" }],
+    ["cidade", { cidade: "Mogi Guaçu" }],
+    ["email", { email: "contato@cdc.com" }],
+  ])(`Deve alterar o campo %s`, async (chave, param) => {
+    const requisicao = { request };
+    const spy = jest.spyOn(requisicao, "request");
+    await requisicao
+      .request(app)
+      .put(`/editoras/${idResposta}`)
+      .send(param)
+      .expect(204);
+
+    expect(spy).toHaveBeenCalled();
+  });
+});
+
+describe("GET em /editoras/id", () => {
+  test.each([
+    ["nome", { nome: "ACD" }],
+    ["cidade", { cidade: "Mogi Guaçu" }],
+    ["email", { email: "contato@cdc.com" }],
+  ])(`Verifica se o campo %s foi alterado`, async (chave, param) => {
+    const resposta = await request(app)
+      .get(`/editoras/${idResposta}`)
+      .set("Accept", "application/json")
+      .expect("content-type", /json/)
+      .expect(200);
+
+    expect(resposta.body.chave).toEqual(param.chave);
+  });
+});
+
+describe("DELETE em /editoras/id", () => {
+  it("Deve deletar o registro adicionado", async () => {
+    await request(app).delete(`/editoras/${idResposta}`).expect(200);
+  });
+});
