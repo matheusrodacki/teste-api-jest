c8efa13aeb9fbc153145d4374ab455b98a51d716
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2023-10-11 06:19:44 -0300

    feat: Add tests for authService.cadastrarUsuario
    - Added a test to check if the user must have a password
    - Added a test to check if the user's password is encrypted
    - Added a test to check if a user with a duplicate email cannot be registered
    - Added a test to check if a message is returned informing that the registration was successful
    - Added a test to check if the registered data is returned

--- src/test/services/authService.test.js
+++ src/test/services/authService.test.js
@@ -1,60 +1,86 @@
 import { describe, expect, it, jest } from "@jest/globals";
 import AuthService from "../../services/authService.js";
+import Usuario from "../../models/usuario.js";
 import bcryptjs from "bcryptjs";
 
 const authService = new AuthService();
 
 describe("Test authService.cadastrarUsuario", () => {
   it("O usuário deve ter nome e senha!", async () => {
-    const usuarioTeste = {
+    const usuarioSemSenha = {
       nome: "Rafael",
       email: "rapha@g.com",
     };
-    const usuarioSalvo = authService.cadastrarUsuario(usuarioTeste);
+    const usuarioSalvo = authService.cadastrarUsuario(usuarioSemSenha);
 
     await expect(usuarioSalvo).rejects.toThrowError(
       "A senha do usuario é obritatória!"
     );
   });
 
-  it("Não pode ser cadastrado um usuário com e-mail duplicado", async () => {
-    const usuarioDuplicado = {
-      nome: "Raphael",
-      email: "raphael@teste.com.br",
-      senha: "123456",
-    };
-
-    const resultado = authService.cadastrarUsuario(usuarioDuplicado);
-
-    await expect(resultado).rejects.toThrowError("O email já esta cadastrado!");
-  });
-
   it("A senha do usuario deve ser criptografada. ", async () => {
     const usuarioTeste = {
-      nome: "Rafael",
-      email: "rapha@g.com",
-      senha: "12345",
+      nome: "John Doe",
+      email: "johndoe@example.com",
+      senha: "senha123",
     };
-    authService.cadastrarUsuario = jest.fn().mockReturnValueOnce({
-      message: "usuario criado",
-      content: {
-        id: 15,
-        nome: "Rafael",
-        email: "rapha@g.com",
-        senha: `${await bcryptjs.hash("12345", 8)}`,
-        created_at: "2023-10-05",
-        updated_at: "2023-10-05",
-      },
-    });
 
     const usuarioSalvo = await authService.cadastrarUsuario(usuarioTeste);
 
     const senhaComparada = await bcryptjs.compare(
-      "12345",
+      "senha123",
       usuarioSalvo.content.senha
     );
 
     expect(senhaComparada).toStrictEqual(true);
+
+    await Usuario.excluir(usuarioSalvo.content.id);
+  });
+
+  it("Não pode ser cadastrado um usuário com e-mail duplicado", async () => {
+    const usuarioTeste = {
+      nome: "Raphael",
+      email: "raphael@teste.com.br",
+      senha: "123456",
+    };
+    const usuarioSalvo = authService.cadastrarUsuario(usuarioTeste);
+
+    await expect(usuarioSalvo).rejects.toThrowError(
+      "O email já esta cadastrado!"
+    );
+  });
+
+  it("Deve ser retornada uma mensagem informando que o cadastro foi realizado;", async () => {
+    const usuarioTeste = {
+      nome: "John Doe",
+      email: "johndoe@example.com",
+      senha: "senha123",
+    };
+    const usuarioSalvo = await authService.cadastrarUsuario(usuarioTeste);
+
+    expect(usuarioSalvo.message).toEqual("usuario criado");
+    await Usuario.excluir(usuarioSalvo.content.id);
+  });
+  it("Deve restornar os dados cadastrados", async () => {
+    const usuarioTeste = {
+      nome: "John Doe",
+      email: "johndoe@example.com",
+      senha: "senha123",
+    };
+    const usuarioSalvo = await authService.cadastrarUsuario(usuarioTeste);
+
+    expect(usuarioSalvo).toEqual(
+      expect.objectContaining({
+        message: expect.any(String),
+        content: expect.objectContaining({
+          id: expect.any(Number),
+          ...usuarioTeste,
+          created_at: expect.any(String),
+          updated_at: expect.any(String),
+        }),
+      })
+    );
+    await Usuario.excluir(usuarioSalvo.content.id);
   });
 });
 
